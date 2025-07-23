4b9bdd2161ee78a2726fe7fc72c4e08028098da3
Maks4529 <max555gos@gmail.com>
2025-07-23 13:30:11 +0300

    Add middleware errorHandlers

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
+const { errorHandlers } = require('./middleware');
 
 const app = express();
 
 app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use('/api', router);
-
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
 
 module.exports = app;
