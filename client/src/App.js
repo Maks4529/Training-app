4cd3cf55816956847dbc9b73945f6fbd0eb933b9
Maks4529 <max555gos@gmail.com>
2025-07-29 14:55:57 +0300

    Add routing on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RegistrationPage from './pages/RegistrationPage';
+import CreateTrainingPage from './pages/CreateTrainingPage';
+import LoginPage from './pages/LoginPage';
+import MorePage from './pages/MorePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/registration' Component={RegistrationPage} />
+          <Route path='/create' Component={CreateTrainingPage} />
+          <Route path='/login' Component={LoginPage} />
+          <Route path='/trainings' Component={MorePage} />
+          <Route path='*' Component={NotFoundPage} />
+        </Routes>
+      </Router>
     </div>
   );
 }
