11b91b0263b18542b30cf90200f7e734986eb6f8
Maks4529 <max555gos@gmail.com>
2025-07-29 15:10:54 +0300

    Add home page

--- client/src/components/NavMenu/index.jsx
+++ client/src/components/NavMenu/index.jsx
@@ -1,6 +1,25 @@
+import {NavLink} from 'react-router-dom';
+import styles from './NavMenu.module.sass';
+
 function NavMenu() {
   return (
-    <div>NavMenu</div>
+    <>
+    <nav className={styles.navMenu}>
+        <img className={styles.logo} src='/images/logo.png' alt='logo' />
+        <p className={styles.slogan}>Shape Your Skills. Strengthen Your Body.</p>
+        <ul className={styles.navList}>
+            <li className={styles.navItem}>
+                <NavLink className={styles.navLink} to='/create'>Create training</NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink className={styles.navLink} to='/registration'>Registration on training</NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink className={styles.navLink} to='/login'>LogIn</NavLink>
+            </li>
+        </ul>
+    </nav>
+    </>
   )
 }
 
--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,8 +1,18 @@
 import NavMenu from "../../components/NavMenu";
+import { NavLink } from "react-router-dom";
+import styles from './HomePage.module.sass';
 
 function HomePage() {
   return (
-    <div><NavMenu/></div>
+    <div className={styles.homePage}>
+        <img className={styles.bg} src="/images/bg.jpg" alt="background" />
+        <NavMenu />
+        <div className={styles.animateBlock}>
+        <h1 className={styles.mainTitle}>Full range of services</h1>
+        <p className={styles.subTitle}>Discover everything you need in a local gym, and nothing you don't.</p>
+        <NavLink className={styles.btnMore} to='/trainings'>More</NavLink>
+        </div>
+    </div>
   )
 }
 
