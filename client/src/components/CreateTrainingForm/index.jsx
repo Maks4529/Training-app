522cb865b4fea5836cc91a8ecd30492dda6da679
Maks4529 <max555gos@gmail.com>
2025-08-02 11:13:08 +0300

    Add create training page and create training form

--- client/src/components/CreateTrainingForm/index.jsx
+++ client/src/components/CreateTrainingForm/index.jsx
@@ -1,7 +1,51 @@
-function CreateTrainingForm() {
+import {Formik, Form} from 'formik';
+import {connect} from 'react-redux';
+import Input from './../inputs';
+import styles from './CreateTrainingForm.module.sass';
+import { createTrainingThunk } from './../../store/slices/trainingsSlice';
+
+function CreateTrainingForm({createTraining}) {
+  const initialValues = {
+    title: '',
+    description: '',
+  };
+
+  const classes = {
+      input: styles.input,
+      error: styles.error,
+      valid: styles.valid,
+      invalid: styles.invalid,
+    };
+
+    const handleSubmit = (values, formikBag) => {
+      createTraining({
+        title: values.title,
+        description: values.description,
+      });
+
+      formikBag.resetForm();
+    };
+
   return (
-    <div>CreateTrainingForm</div>
+    <Formik
+    initialValues={initialValues}
+    onSubmit={handleSubmit}
+    >
+      {formikProps => (
+        <Form className={styles.form}>
+          <Input label='Title' type='text' name='title' placeholder='Training title...'
+          classes={classes} />
+          <Input label='Description' type='text' name='description' placeholder='Training description...'
+          classes={classes} />
+          <button type='submit' className={styles.submitBtn}>Save</button>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
-export default CreateTrainingForm
+const mapDispatchToProps = dispatch => ({
+  createTraining: data => dispatch(createTrainingThunk(data)),
+});
+
+export default connect(null, mapDispatchToProps) (CreateTrainingForm);
--- client/src/pages/CreateTrainingPage/index.jsx
+++ client/src/pages/CreateTrainingPage/index.jsx
@@ -1,8 +1,14 @@
 import CreateTrainingForm from "../../components/CreateTrainingForm";
+import HomeButton from '../../components/HomeButton';
+import styles from './CreateTrainingPage.module.sass';
 
 function CreateTrainingPage() {
   return (
-    <div><CreateTrainingForm/></div>
+    <div className={styles.createTrainingPage}>
+      <HomeButton className={styles.homeBtn} />
+      <h2>Create new training</h2>
+      <CreateTrainingForm/>
+    </div>
   )
 }
 
