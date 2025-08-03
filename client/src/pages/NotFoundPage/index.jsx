1440b27e7d2f219e16b776afe6900b5aa0ba5be3
Maks4529 <max555gos@gmail.com>
2025-08-03 13:16:38 +0300

    Add not found page

--- client/src/pages/NotFoundPage/index.jsx
+++ client/src/pages/NotFoundPage/index.jsx
@@ -1,6 +1,12 @@
+import HomeButton from '../../components/HomeButton';
+import styles from './NotFoundPage.module.sass';
+
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div className={styles.notFoundPage}>
+      <HomeButton />
+      <h2>Not Found...</h2>
+    </div>
   )
 }
 
